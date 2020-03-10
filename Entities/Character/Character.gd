4f8ef36e7fcd28a6574ccffa0fa5db5edf9f056b
Michal Biernacki <userofficeit@gmail.com>
2020-03-10 21:11:11 +0100

    Add moving players in game (should be working xd)

--- Entities/Character/Character.gd
+++ Entities/Character/Character.gd
@@ -1,84 +1,22 @@
 extends KinematicBody2D
 
-
 export (int) var move_speed
-export (String) var character_class
-export (float) var rotate_cooldown
-export (int) var rotate_speed
-export (float) var hp
-export (int) var bullet_speed
-export (int) var bullet_damage
 
-var rotate_direction = 1  #1 = right, -1 = left
-var can_rotate = true
-var inside_standing_circle = false
-onready var id = int(self.get_name())
-
-remote var mouse_pos = Vector2()
-remote var shoot_pressed = false
-remote var switch_pressed = false
+var players_info = {}
 
 func _process(delta):
-	handle_inputs()
-	move(delta)
-	rotate_player(delta)
-	get_parent().players[id] = {"position": position, "rotation": $Body.rotation, "updated": true}
+	handle_inputs(delta)
+	update_world()
 
+func handle_inputs(delta):
+	var mouse_pos = get_viewport().get_mouse_position()
+	var player_input = {"mouse_pos": mouse_pos, "delta": delta}
+	rpc_unreliable_id(1, "process_input", player_input)
 	
-func handle_inputs():
-	if switch_pressed:
-		rotate_direction = -rotate_direction
-		switch_pressed = false
-		can_rotate = false
-		$RotateCooldown.start(rotate_cooldown)
-		
-	if shoot_pressed:
-		shoot_pressed = false
-		#shoot()
-
-func move(delta):
-	if !inside_standing_circle:
-		var direction = mouse_pos - position
-		var velocity_vector = direction.normalized() * move_speed * delta
-		move_and_collide(velocity_vector)
-		get_parent().players[id][position] = position
-		rset_id(id, "player_position", self.position)
-
-func rotate_player(delta):
-	$Body.rotate(rotate_direction * rotate_speed * delta)
-	get_parent().players[id][rotation] = $Body.rotation
-	rset_id(id, "player_rotation", $Body.rotation)
+remotesync func update_players_info(id, player_info):
+	players_info[id] = player_info
 	
-"""
-func shoot():
-	var bullet = Bullet.instance()
-	bullet.global_position = get_node("Body/Rifle/Bullet_spawn").global_position
-	bullet.init(bullet_speed, bullet_damage, Vector2(0, -1).rotated($Body.rotation),
-				$Body.rotation, self.get_name())
-	get_parent().add_child(bullet)
+func update_world():
+	for player_id in players_info:
+		get_parent().get_node(str(player_id)).global_position = players_info[player_id]["position"]
 	
-func send_dictionary():
-	var players_dict = get_parent().players
-	for player_id in players_dict:
-		if players_dict[player_id]["updated"] == false:
-			break
-		rset("players", players_dict)
-
-
-func got_shot(dmg):
-	if dmg >= hp:
-		hp = 0
-		queue_free()
-	else:
-		hp -= dmg
-
-
-func RotateCooldown_timeout():
-	can_rotate = true
-	$RotateCooldown.stop()
-
-func StandingCircle_mouse_entered():
-	inside_standing_circle = true
-
-func StandingCircle_mouse_exited():
-	inside_standing_circle = false"""
--- Scripts/GameManagement/GameManager.gd
+++ Scripts/GameManagement/GameManager.gd
@@ -3,7 +3,7 @@ extends Node
 var GameSession = preload("res://Scenes/World/World.tscn")
 var Player = preload("res://Entities/Character/Player/Player.tscn")
 var world
-var player_info = {}
+var players_info = {}
 
 func prepare_game():
 	randomize()
@@ -20,8 +20,23 @@ remotesync func initialize_players():
 	print("initializing players...")
 	for p in LobbyManager.players:
 		var player = Player.instance()
-		player.set_name(p)
+		player.set_name(str(p))
 		player.global_position = Vector2(randi()%int(get_viewport().size.x), randi()%int(get_viewport().size.y))
-		player_info[p] = {"position": player.global_position}
+		players_info[p] = {"position": player.global_position}
 		world.add_child(player)
 		get_tree().change_scene_to(world)
+		
+remote func process_input(input):
+	var mouse_pos = input['mouse_pos']
+	var delta = input['delta']
+	update(get_tree().get_rpc_sender_id(), mouse_pos, delta)
+	
+func update(id, mouse_pos, delta):
+	var player = get_node("world/" + str(id))
+	var direction = mouse_pos - players_info[id]['position']
+	player.move_and_collide(direction.normalized() * player.move_speed * delta)
+	var player_info = {"position": player.global_position}
+	rpc_unreliable("update_players_info", id, player_info)
+	
+remotesync func update_players_info(id, player_info):
+	players_info[id] = player_info
