3660e9bb0636c10fac897022f555ee30508e6e5a
Michal Biernacki <userofficeit@gmail.com>
2020-03-10 16:23:27 +0100

    Fix lobby
    Now when user exits lobby he is being disconnected from server. When server exits lobby, SceneTree's peer is null

--- Scenes/Lobby/Lobby.gd
+++ Scenes/Lobby/Lobby.gd
@@ -1,15 +1,28 @@
 extends Control
 
 var PlayerLabel = preload("res://Scenes/Lobby/PlayerLabel.tscn")
-onready var parent_container = get_node("MarginContainer/VBoxContainer")
+onready var labels_container = get_node("MarginContainer/VBoxContainer")
+onready var scene_tree = get_tree()
+
+func _ready():
+	StartGameBtn_configure()
+
+func StartGameBtn_configure():
+	if !scene_tree.is_network_server():
+		$MarginContainer/StartGameBtn.disabled = true
 
 func add_item(player_name):
 	var new_label = PlayerLabel.instance()
-	parent_container.add_child(new_label)
+	labels_container.add_child(new_label)
 	new_label.set_text(player_name)
 	
 func remove_item(player_name):
-	for c in parent_container.get_children():
+	for c in labels_container.get_children():
 		if c.text == player_name:
-			parent_container.remove_child(c)
+			labels_container.remove_child(c)
 			break
+
+func _on_QuitLobbyBtn_pressed():
+	scene_tree.change_scene("res://Scenes/MainMenu/MainMenu.tscn")
+	LobbyManager.disconnect_from_server(LobbyManager.my_id)
+	queue_free()
--- Scripts/GameManagement/LobbyManager.gd
+++ Scripts/GameManagement/LobbyManager.gd
@@ -1,7 +1,10 @@
 extends Node
 
 var Player = preload("res://Entities/Character/Player/Player.tscn")
+
 var lobby
+var peer
+onready var scene_tree = get_tree()
 
 const PORT = 2137
 const LOBBY_SIZE = 5
@@ -12,33 +15,32 @@ var server_ip
 
 
 func create_server():
-	var peer = NetworkedMultiplayerENet.new()
+	peer = NetworkedMultiplayerENet.new()
 	peer.create_server(PORT, LOBBY_SIZE)
-	get_tree().set_network_peer(peer)
+	scene_tree.set_network_peer(peer)
 	peer.connect("peer_connected", self, "_on_peer_connected")
 	peer.connect("peer_disconnected", self, "_on_peer_disconnected")
+	print(scene_tree.get_network_unique_id())
 	create_lobby()
 	
 func create_client():
-	var peer = NetworkedMultiplayerENet.new()
-	get_tree().connect("connection_failed", self, "onConFail")
+	peer = NetworkedMultiplayerENet.new()
 	peer.create_client(server_ip, PORT)
-	get_tree().set_network_peer(peer)
+	scene_tree.set_network_peer(peer)
 	peer.connect("peer_connected", self, "_on_peer_connected")
 	peer.connect("peer_disconnected", self, "_on_peer_disconnected")
-	create_lobby()
-		
-func onConFail():
-	print("skkkkkk")
-
+	
 func _on_peer_connected(id):
+	print(scene_tree.get_network_unique_id())
+	if !scene_tree.is_network_server():
+		create_lobby()
 	rpc_id(id, "register_player", my_name)
 	
 func _on_peer_disconnected(id):
 	unregister_player(players[id])
 	
 remote func register_player(new_player_name):
-	var id = get_tree().get_rpc_sender_id()
+	var id = scene_tree.get_rpc_sender_id()
 	players[id] = new_player_name
 	
 	if id != 1: #for client screen
@@ -52,12 +54,17 @@ func create_lobby():
 	var Lobby = load("res://Scenes/Lobby/Lobby.tscn")
 	lobby = Lobby.instance()
 	add_child(lobby)
-	get_tree().change_scene_to(lobby)
+	scene_tree.change_scene_to(lobby)
 	
-	if get_tree().get_network_unique_id() != 1: #for server screen
+	if !scene_tree.is_network_server(): #for server screen
 		lobby.add_item(my_name)
-	
 
-#Client code here v
-
+remote func disconnect_from_server(id):
+	if scene_tree.is_network_server():
+		if id != 1:
+			scene_tree.get_network_peer().disconnect_peer(id, true)
+		else:
+			scene_tree.network_peer = null #when server quits the lobby its removing server
+	else:
+		rpc_id(1, "disconnect_from_server", scene_tree.get_network_unique_id())
 
