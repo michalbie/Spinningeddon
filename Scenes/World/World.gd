cb684e891238285045702510ffb4d6194af7090d
Michal Biernacki <userofficeit@gmail.com>
2020-03-28 14:23:05 +0100

    Add cameras system
    Added camera changing after death

--- Scenes/World/World.gd
+++ Scenes/World/World.gd
@@ -17,7 +17,7 @@ remotesync func spawn_bullet(player_id):
 	bullet.global_position = player.get_node("Body/Bullet_spawn").global_position
 	bullet.init(player.bullet_speed, player.bullet_damage, 
 				Vector2(0, -1).rotated(player.get_node("Body").rotation), 
-				player.get_node("Body").rotation, player.bullet_range)
+				player.get_node("Body").rotation, player.bullet_range, player_id)
 	GameManager.bullets_info[bullet.get_name()] = {"position": bullet.global_position, "rotation": bullet.get_node("Sprite").rotation}
 	GameManager.bullets_count += 1
 	
@@ -26,7 +26,15 @@ remotesync func delete_bullet(bullet_name):
 	get_node(bullet_name).queue_free()
 	remove_child(get_node(bullet_name))
 		
-remotesync func kill_player(player_name):
+remotesync func kill_player(player_name, killer_name):
 	GameManager.delete_player(player_name)
+	if player_name == get_tree().get_network_unique_id() or get_tree().get_network_unique_id() in get_node(str(player_name)).observers_list:
+		get_random_camera()
 	get_node(str(player_name)).queue_free()
+	
+func get_random_camera():
+	randomize()
+	var random_index = randi() % GameManager.players_info.size()
+	self.get_node(str(GameManager.players_info.keys()[random_index])).get_node("Camera2D").make_current()
+	self.get_node(str(GameManager.players_info.keys()[random_index])).observers_list.append(get_tree().get_network_unique_id())
 
