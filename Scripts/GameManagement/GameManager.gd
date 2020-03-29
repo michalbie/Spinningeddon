87c2e79e38be60570f9b507f3f47aaa847feb4e2
Bartosz <62819066+Deaponn@users.noreply.github.com>
2020-03-29 01:22:17 +0100

    Added random spawnpoints

--- Scripts/GameManagement/GameManager.gd
+++ Scripts/GameManagement/GameManager.gd
@@ -10,7 +10,6 @@ remote var players_info = {}
 remote var bullets_info = {}
 remote var bullets_count = 0
 
-
 func _physics_process(delta):
 	if in_game:
 		update_world()
@@ -34,9 +33,15 @@ remotesync func initialize_players():
 		var player = picked_class_scene.instance()
 		player.set_name(str(p))
 		world.add_child(player)
-		player.position = Vector2(randi()%int(get_viewport().size.x), randi()%int(get_viewport().size.y))
+		player.position = random_spawnpoint()
 		players_info[p] = {"position": player.position, "body_rotation": player.get_node("Body").rotation}
 		get_tree().change_scene_to(world)
+		
+func random_spawnpoint():
+	var x_axis = [2054, 750, 3145, 4858, 8208, 11649, 2137, 7730, 5983, 3971, 6275, 9049, 11839, 10444, 9073, 6675, 12331]
+	var y_axis = [1510, 5000, 6606, 9790, 10184, 8987, 9001, 7470, 7184, 3498, 1179, 1604, 1826, 4098, 5777, 4295, 6470]
+	var number = randi()%17
+	return Vector2(x_axis[number], y_axis[number])
 		
 func update_world():
 	update_players()
@@ -49,8 +54,9 @@ func update_players():
 
 func update_bullets():
 	for bullet in bullets_info:
-		world.get_node(bullet).global_position = bullets_info[bullet]['position']
-		world.get_node(bullet + "/Sprite").rotation = bullets_info[bullet]['rotation']
+		if world.get_node(bullet) != null:
+			world.get_node(bullet).global_position = bullets_info[bullet]['position']
+			world.get_node(bullet + "/Sprite").rotation = bullets_info[bullet]['rotation']
 
 func delete_player(player_name):
 	players_info.erase(player_name)
