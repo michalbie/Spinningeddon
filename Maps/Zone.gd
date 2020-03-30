e3b2620b3172c1507e21aff37e4a389c6e53cafa
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-03-31 00:54:37 +0200

    Added exported variables in Zone.gd
    Time that zone takes to shrink to its minimal size, damage dealt to player in the zone and time between each instances of damage are all exported variables editable in Zone.tscn now

--- Maps/Zone.gd
+++ Maps/Zone.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
+export var time_to_shrink = 120
+export var damage_dealt = 50
+export var damage_interval = 2
+
 func _ready():
 	var zone_tween = get_node("ZoneTween")
 	zone_tween.interpolate_property($SafeZone, "texture_scale",
-	40, 3, 120, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	40, 3, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	zone_tween.start()
 	
 	var area_tween = get_node("AreaTween")
 	area_tween.interpolate_property($AreaZone/SafeArea.shape, "radius",
-	9200, 690, 120, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	9200, 690, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	area_tween.start()
 	
 func _on_Game_Started():
@@ -16,7 +20,7 @@ func _on_Game_Started():
 		var timer = Timer.new()
 		var player_node = GameManager.world.get_node(str(player))
 		timer.connect("timeout", self, "_on_DamageTimer_timeout", [player_node])
-		timer.set_wait_time(2)
+		timer.set_wait_time(damage_interval)
 		timer.set_name(str(player))
 		add_child(timer)
 
@@ -29,5 +33,5 @@ func _on_Area2D_body_entered(body):
 		get_node(body.get_name()).stop()
 
 func _on_DamageTimer_timeout(player):
-	player.got_shot(50, "Fog")
+	player.got_shot(damage_dealt, "Fog")
 
