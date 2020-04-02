d3aaeadf433a8d6be76034c3b8eec0aa9eec5ae7
Michal Biernacki <userofficeit@gmail.com>
2020-04-02 16:52:01 +0200

    Fix bugs related to Zone
    Previously, everybody was sending a signal to everybody when entered a fog. Now only server is responsible for that, as it should be

--- Maps/BattleRoyaleMap/Zone.gd
+++ Maps/BattleRoyaleMap/Zone.gd
@@ -19,25 +19,27 @@ func _on_Game_Started():
 	for player in LobbyManager.players:
 		var timer = Timer.new()
 		var player_node = GameManager.world.get_node(str(player))
-		timer.connect("timeout", self, "_on_DamageTimer_timeout", [player_node])
+		timer.connect("timeout", self, "_on_DamageTimer_timeout", [str(player)])
 		player_node.connect("player_died", self, "delete_timer", [str(player)])
 		timer.set_wait_time(damage_interval)
 		timer.set_name(str(player))
 		add_child(timer)
 
 func _on_Area2D_body_exited(body):
-	if body.has_method("got_shot"):
-		if GameManager.players_info.has(int(body.get_name())):
-			var target = get_node(body.get_name())
-			target.start()
-		
+	if get_tree().is_network_server():
+		if body.has_method("got_shot"):
+			if GameManager.players_info.has(int(body.get_name())):
+				var target = get_node(body.get_name())
+				target.start()
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("got_shot"):
-		get_node(body.get_name()).stop()
+	if get_tree().is_network_server():
+		if body.has_method("got_shot"):
+			get_node(body.get_name()).stop()
 
-func _on_DamageTimer_timeout(player):
-	player.got_shot(damage_dealt, "Fog")
+func _on_DamageTimer_timeout(player_name):
+	if GameManager.world.get_node(player_name) != null:
+		GameManager.world.get_node(player_name).got_shot(damage_dealt, "Fog")
 	
 func delete_timer(name):
 	get_node(name).stop()
