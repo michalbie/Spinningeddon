86b75d2d083af7bad6ae98433f6d95c30b58efd5
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-04-04 14:42:00 +0200

    Moved player label logic into GameplayInfo.gd
    Also changed scene layout and font PlayerName.tscn (the label scene)

--- Scripts/GameManagement/GameManager.gd
+++ Scripts/GameManagement/GameManager.gd
@@ -4,7 +4,6 @@ signal game_started()
 
 var GameSession = preload("res://Scenes/World/World.tscn")
 var Lobby = preload("res://Scenes/Lobby/Lobby.tscn")
-var PlayerLabel = preload("res://Entities/Character/Player/PlayerName.tscn")
 var world
 var occupied_spawnpoints = []
 
@@ -22,6 +21,7 @@ func prepare_game():
 	rpc("initialize_world")
 	rpc("initialize_players")
 	rset("in_game", true)
+	
 
 remotesync func initialize_world():
 	print("initializing world...")
@@ -32,6 +32,7 @@ remotesync func initialize_world():
 	if get_tree().is_network_server():
 		var zone = world.get_node("BattleRoyaleMap/Zone")
 		connect("game_started", zone, "_on_Game_Started")
+		
 
 remotesync func initialize_players():
 	print("initializing players...")
@@ -39,15 +40,11 @@ remotesync func initialize_players():
 		var picked_class_scene = Globals.classes[LobbyManager.players[p]["class"]]["scene"]
 		var player = picked_class_scene.instance()
 		player.set_name(str(p))
-		var player_label = PlayerLabel.instance()
-		player_label.set_name("label_" + str(p))
-		player_label.get_node("Background/Name").text = LobbyManager.players[p]["name"]
 		world.add_child(player)
-		world.add_child(player_label)
 		player.position = random_spawnpoint()
-		player_label.position = player.position
 		players_info[p] = {"position": player.position, "body_rotation": player.get_node("Body").rotation}
 		get_tree().change_scene_to(world)
+	world.gameplay_info.rpc("create_labels")
 	emit_signal("game_started")
 		
 func random_spawnpoint():
@@ -69,7 +66,6 @@ func update_players():
 		if world.get_node(str(player_id)) != null:
 			world.get_node(str(player_id)).position = players_info[player_id]["position"]
 			world.get_node(str(player_id)).rotation = players_info[player_id]["body_rotation"]
-			world.get_node("label_" + str(player_id)).position = players_info[player_id]["position"]
 
 func update_bullets():
 	for bullet in bullets_info:
