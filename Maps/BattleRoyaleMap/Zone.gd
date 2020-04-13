a417a1f65549744f325a2679c566c70a3c34f658
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-04-13 14:22:01 +0200

    Random zone (#43)
    * Decreased zone final size and fixed map borders
    
    Fog now correctly covers map borders
    
    * Added randomized map center

--- Maps/BattleRoyaleMap/Zone.gd
+++ Maps/BattleRoyaleMap/Zone.gd
@@ -5,14 +5,18 @@ export var damage_dealt = 50
 export var damage_interval = 2
 
 func _ready():
+	if get_tree().is_network_server():
+		var center_position = get_random_zone_center()
+		rpc("set_zone_center", center_position)
+	
 	var fog_tween = get_node("FogTween")
 	fog_tween.interpolate_property($SafeZone, "texture_scale",
-	20, 2, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	28, 2, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	fog_tween.start()
 	
 	var safe_area_tween = get_node("SafeAreaTween")
 	safe_area_tween.interpolate_property($AreaZone/SafeArea.shape, "radius",
-	9200, 920, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	12800, 920, time_to_shrink, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	safe_area_tween.start()
 	
 func _on_Game_Started():
@@ -44,3 +48,13 @@ func _on_DamageTimer_timeout(player_name):
 func delete_timer(name):
 	get_node(name).stop()
 	get_node(name).queue_free()
+	
+func get_random_zone_center():
+	randomize()
+	var potential_center_positions = [Vector2(0, 0), Vector2(900, 2000), Vector2(-2200, 1600), Vector2(1400, -3000), Vector2(25, -1100)]
+	var center_position = potential_center_positions[randi() % potential_center_positions.size()]
+	return center_position
+	
+remotesync func set_zone_center(center_position):
+	$SafeZone.position = center_position
+	$AreaZone.position = center_position
