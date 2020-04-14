840f57d6ae0310665ade98b71c24be39715b08c3
Michal Biernacki <userofficeit@gmail.com>
2020-04-14 19:53:03 +0200

    Fix bullets

--- Maps/Objects/share/ObjectBase.gd
+++ Maps/Objects/share/ObjectBase.gd
@@ -7,3 +7,6 @@ func _on_BulletDeathArea_body_entered(body):
 	if get_tree().is_network_server():
 		if body.get_filename() == "res://Entities/Bullet/Bullet.tscn" and object_material != materials.WOOD:
 			GameManager.world.rpc("delete_bullet", body.get_name())
+			
+		elif body.get_filename() == "res://Entities/Bullet/SoldierBullet.tscn":
+			body.distance_traveled += (body.bullet_range - body.distance_traveled) / 2
