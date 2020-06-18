070f389ec0bdeeb475b25a43076bef8b55e0d966
Ethelt <bartlomiej.hada@gmail.com>
2020-06-18 12:58:15 +0200

    Revert "Added healthpacks that spawn when player dies and heal 50 HP upon pickup"
    This reverts commit 6f1a340c6306ce74280e64e7b44c9981a3d8d80c.

--- Entities/Healthpack/Healthpack.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-func _on_Healthpack_body_entered(body):
-	if body.has_method("got_shot"):
-		body.got_shot(-50, "Game")
-		GameManager.world.rpc("delete_healthpack", self.get_name())
